1bba1ea8821c0851172f96526786b2b5096f3803
Le Bo <tegiacmotsung97@gmail.com>
2017-08-22 10:12:33 +0700

    v2.0.1. Sửa lỗi

--- UART/UARTCommunication.c
+++ UART/UARTCommunication.c
@@ -4,11 +4,12 @@
 int main() {
 	enableUART1(1, 0, 0, 1, 5);
 	while (1)
-	{        
+	{
 		char ReceivedChar;
 		__C30_UART = 1;
 		fflush(stdin);
 		ReceivedChar = U1RXREG;
 		__C30_UART = 2;
 		printf("%c", ReceivedChar);
+	}
 }
