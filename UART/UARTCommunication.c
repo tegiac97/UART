cacd08c45f8a336742ef934964a2cd197118f67b
Le Bo <tegiacmotsung97@gmail.com>
2017-08-29 18:31:40 +0700

    v2.2. Su dung ngat(test thu)

--- UART/UARTCommunication.c
+++ UART/UARTCommunication.c
@@ -1,19 +1,25 @@
 #include"uart.h"
 #include<string.h>
 
+char receivedChar;
+
 int main() {
 	enableUART1(1, 0, 0, 1, 5);
 	enableUART2(1, 0, 0, 1, 5);
-	while (1) {
-		char *buffer;
-		while (U1STAbits.URXDA==1)
-		{	
-			*buffer = U1RXREG;
-			printf("%c ",*buffer);
-			__delay_ms(100);
-			buffer++;
-		}
-	}
+	U1STAbits.URXISEL = 00;	//  ngat xay ra khi nhan duoc mot ky tu
+	//__delay_ms(100);	
+//	receivedChar = U1RXREG;
+	IEC0bits.U1RXIE = 1; // bat ngat nhan ky tu
+	while (1)
+	{
 
+	}
+}
+
+void __attribute__((__interrupt__, __auto_psv__)) _U1RXInterrupt(void) {
+	receivedChar = U1RXREG;
+
+	IFS0bits.U1RXIF = 0; // xoa co ngat
+	puts(receivedChar);
 }
 
